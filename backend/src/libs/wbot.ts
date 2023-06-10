93810957f9c0950bd313020bb2c36aaf87090c6e
Gustavolgt <gustavocms16@gmail.com>
2023-06-10 09:20:08 -0300

    alteração wbot

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -5,7 +5,7 @@ import makeWASocket, {
   fetchLatestBaileysVersion,
   LegacyAuthenticationCreds,
   makeInMemoryStore,
-  makeWALegacySocket
+  makeWALegacySocke
 } from "@adiwajshing/baileys";
 
 import { Boom } from "@hapi/boom";
